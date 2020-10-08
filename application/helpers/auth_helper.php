9a3d79190542291947561cfbfda959b7ad8a4667
rochac2lee <cleberli.rocha@gmail.com>
2020-10-08 14:50:22 -0300

    Documentação do Helper Auth

--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -1,10 +1,16 @@
 <?php
 
+/**
+ * A Função permission
+ *
+ * @return object $loggedUser = Dados do Usuário logado
+ */
 function permission() {
   $ci = get_instance();
 
   $loggedUser = $ci -> session -> userdata("loggedUser");
 
+  /** Valida os usuários que não tem sessão ativa  */
   if (!$loggedUser) {
     $ci -> session -> set_flashdata("danger", "Você não está logado no sistema!");
     redirect("login");
--- application/helpers/upload_helper.php
+++ application/helpers/upload_helper.php
@@ -3,41 +3,43 @@
 /**
  * A Função uploadFiles serve para realizar o upload dos arquivos enviados como método post
  *
- * @param  mixed $filesToSend
+ * @param  object $filesToSend = Arquivos enviados por post
  * @return void
  */
 function uploadFiles($filesToSend) {
 
   $filesToSend   = $_FILES['files'];
+
+  /** @var int $numFile recebe a quantidade de arquivos */
   $numFile       = count(array_filter($filesToSend['name']));
 
-  //requisitos
-  $permite 	= array (
+  /** @var array $permite recebe um array com os tipos de arquivo que podem ser enviados  */
+  $permite       = array (
 	
     'application/msword',
     'application/msword',
-  
+
     'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
     'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
     'application/vnd.ms-word.document.macroEnabled.12',
     'application/vnd.ms-word.template.macroEnabled.12',
-  
+
     'application/vnd.ms-excel',
     'application/vnd.ms-excel',
     'application/vnd.ms-excel',
-  
+
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
     'application/vnd.ms-excel.sheet.macroEnabled.12',
     'application/vnd.ms-excel.template.macroEnabled.12',
     'application/vnd.ms-excel.addin.macroEnabled.12',
     'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
-  
+
     'application/vnd.ms-powerpoint',
     'application/vnd.ms-powerpoint',
     'application/vnd.ms-powerpoint',
     'application/vnd.ms-powerpoint',
-  
+
     'application/vnd.openxmlformats-officedocument.presentationml.presentation',
     'application/vnd.openxmlformats-officedocument.presentationml.template',
     'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
@@ -45,16 +47,18 @@ function uploadFiles($filesToSend) {
     'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
     'application/vnd.ms-powerpoint.template.macroEnabled.12',
     'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
-  
+
     'application/vnd.ms-access',
     
     'application/pdf', 'audio/mp3', 'audio/ogg', 'image/jpeg', 'image/jpg', 'image/gif', 'image/png');
+
+  /** @var int $maxSize = Tamanho do arquivo suportado */
   $maxSize	= 1024 * 1024 * 24;
 
-  //pasta
+ /** @var string $folder = Local onde serão armazenadas os arquivos */
   $folder = './uploads';
 
-		//Faz o upload de multiplos arquivos
+    /** Envia múltiplos arquivos para pasta de destino */
 		for ($count = 0; $count < $numFile; $count++) {
 			$name	 = $filesToSend['name'][$count];
 			$type	 = $filesToSend['type'][$count];
@@ -62,11 +66,10 @@ function uploadFiles($filesToSend) {
 			$error = $filesToSend['error'][$count];
 			$tmp	 = $filesToSend['tmp_name'][$count];
 
-			//$data['sendedFile'] += $name;
       $sendedFile = $name;
 
-      //armazena os arquivos na pasta especificada
-      if (move_uploaded_file($tmp, $folder.'/'.$sendedFile)) {$mensagem = "Sucesso!";} else {$mensagem = "deu ruim";};
+      /** armazena os arquivos na pasta especificada */ 
+      move_uploaded_file($tmp, $folder.'/'.$sendedFile);
     }
 
 }
