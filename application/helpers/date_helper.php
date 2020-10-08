7f1a2b8dbae6464761785b202232ff7f4d7991bd
rochac2lee <cleberli.rocha@gmail.com>
2020-10-08 14:53:01 -0300

    Documentação do Helper Date

--- application/helpers/date_helper.php
+++ application/helpers/date_helper.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * A Função dateTime serve para retornar a hora servidor
+ *
+ * @return string $time = Hora
+ */
 function dateTime() {
 
   date_default_timezone_set('America/Sao_Paulo');
